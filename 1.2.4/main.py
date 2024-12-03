8c3d22adf0fb5f811349a710698a2ce3c7c68649
zyon <zyon.taylor@vtsd.com>
2024-12-03 12:28:52 -0500

    1-2 day

--- 1.2.4/main.py
+++ 1.2.4/main.py
@@ -1,4 +1,5 @@
 import turtle as trtl
+import random as rand
 
 # intialize variables
 wn = trtl.Screen()
@@ -18,23 +19,31 @@ maze_painter.speed(0)
 # turn left
 # increment length
 # repeat
-def draw_barrier():
-    maze_painter.right(90)
-    maze_painter.forward(path_width)
-    maze_painter.backward(path_width)
-    maze_painter.left(90)
+def draw_barrier(pos):
+  maze_painter.forward(pos)
+  maze_painter.left(90)
+  maze_painter.forward(path_width)
+  maze_painter.backward(path_width)
+  maze_painter.right(90)
 
-for wall in range(20):
+ # randomize location of doors and barriers in wall
+door = rand.randint(path_width*2, (length - path_width*2))
+barrier = rand.randint(path_width*2, ( length  - path_width*2))
+ # if a door and barrier would be rendered on top of each other, get a new value
+while abs(door - barrier) < path_width:
+  door = rand.randint(path_width*2, (length- path_width*2))
+
+
+for wall in range(21):
     maze_painter.forward(length/3)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
-    if(wall > 5):
-     draw_barrier()
+    if wall > 5:
+     draw_barrier(pow)
     maze_painter.forward(length- path_width - (length/3))
     maze_painter.left(90)
     length += 15
-
 
 
 
